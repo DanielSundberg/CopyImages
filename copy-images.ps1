19fe52af137cb14153c51abc53c0b99a26d9d4d6
Daniel Sundberg <daniel.sundberg@gmx.com>
2014-11-11 19:57:02 +0100

    Added possibility to use dates when copying

--- copy-images.ps1
+++ copy-images.ps1
@@ -1,23 +1,31 @@
-# Förbättringar
-# -Hantera raw-filer
-# -Kommandorads-parametrar för $indir och $outdir
-# -Starta kommando-prompt från någon typ av event
-#  när man stoppar in minneskort...
-# -Hantera bilder från sony-kameran
-# -Förbättra dry-run output
-# -Logga till fil som default
-# -Log levels
+# Possible improvements
+# -Handle more raw file formats
+# -Start cmdlet when memory card is inserted
+# -Handle more camera models
+# -Improve dry-run output
+# -Log to file as default
+# -Log levels for debugging
 
 
 param (
     [switch]$dryRun = $false,
     [string]$indir = "e:\",
     [string]$outdir = "c:\bilder2",
-	[switch]$isNef = $false, 
-	[switch]$printConfig = $false
+	[switch]$raw = $false, 
+	[switch]$printConfig = $false,
+	[switch]$today = $false,
+	[string]$from = "",
+	[string]$to = ""
  )
 
-if ($isNef)
+trap
+{
+	write-output $_
+	exit 1
+}
+ 
+# Raw files or jpg
+if ($raw)
 {
 	$type = "*.nef"
 	$outdir = join-path -Path $outdir -ChildPath "Raw"
@@ -27,11 +35,33 @@ else
 	$type = "*.jpg"
 }
  
+# Parse dates
+$fromDate = [DateTime]::MinValue
+if ($from.length -gt 0)
+{
+	$fromDate = ([DateTime]::Parse($from)).date
+}
+$toDate = [DateTime]::MaxValue
+if ($to.length -gt 0)
+{
+	$toDate = ([DateTime]::Parse($to)).date
+}
+
+# Today overrides other date filters
+if ($today)
+{
+	$fromDate = (get-date).date
+	$toDate = (get-date).date.AddDays(1)
+}
+ 
 echo "Starting..."
 echo "In dir:    $indir"
 echo "Out dir:   $outdir"
 echo "File type: $type"
 echo "Dry run:   $dryRun"
+echo "Today:     $today"
+echo "From:      $fromDate"
+echo "To:        $toDate"
 
 if ($printConfig)
 {
@@ -49,30 +79,33 @@ foreach ($item in $files)
 	echo "$i of $nofFiles"
 	$i += 1
 	$date = $item | get-date
-	$shortDate = $date.ToShortDateString()
-	$fullName = $item.FullName
-	$name = $item.Name
 	
-	
-	$destPath = join-path -Path $outdir -ChildPath $shortDate
+	if (($date -gt $fromDate) -and ($date -lt $toDate))
+	{
+		$shortDate = $date.ToShortDateString()
+		$fullName = $item.FullName
+		$name = $item.Name
+		
+		$destPath = join-path -Path $outdir -ChildPath $shortDate
 
-	if (!(Test-Path -Path $destPath))
-	{
-		echo "Creating directory $destPath..."
-		if (!$dryRun)
+		if (!(Test-Path -Path $destPath))
 		{
-			mkdir $destPath
+			echo "Creating directory $destPath..."
+			if (!$dryRun)
+			{
+				mkdir $destPath
+			}
 		}
-	}
-	
-	$destFile = join-path -Path $destPath -ChildPath $name
-	
-	if (!(Test-Path -Path $destFile))
-	{
-		echo "Copying $fullName to $destFile..."
-		if (!$dryRun)
+		
+		$destFile = join-path -Path $destPath -ChildPath $name
+		
+		if (!(Test-Path -Path $destFile))
 		{
-			copy-item $fullName $destFile
+			echo "Copying $fullName to $destFile..."
+			if (!$dryRun)
+			{
+				copy-item $fullName $destFile
+			}
 		}
 	}
 }
